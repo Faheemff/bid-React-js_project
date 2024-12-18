37e1a6eecc01a96d6e56d055f7c12939d4078cd3
Faheemff <mfaheemweb@gmail.com>
2024-12-18 21:48:13 +0500

    add

--- src/Components/Auth/Login.jsx
+++ src/Components/Auth/Login.jsx
@@ -6,11 +6,22 @@ const Login = () => {
         <>
             <main className='h-screen w-full bg-black text-white'>
                 <header className='px-10 py-6 bg-red-800 px-'>
-                    <div><h1>Faheem</h1></div>
+                    <div><h1>Fah</h1></div>
                 </header>
-                <section className='flex justify-between w-full bg-white'>
-                    <div className='h-[500px] bg-green-600 w-[50%]'></div>
-                    <div className='h-[500px] bg-red-500 w-[50%]'></div>
+                <section className='flex justify-between w-full bg-white px-10'>
+                    <div className='h-[500px] bg-green-600 w-[500px]'></div>
+                    <div className='h-[550px] bg-red-500 text-black px-16 py-8 w-[500px] rounded-[10px]'>
+                        <h1 className='text-[40px] font-semibold'>Log In</h1>
+                        <form className=''>
+                            <input className='w-full rounded-[5px] px-5 py-3 mb-4' type="email" placeholder='Enter your email'/>
+                            <br />
+                            <input className='w-full rounded-[5px] px-5 py-3' type="password" placeholder='Enter your password'/>
+                            <div className='flex gap-5 items-center'>
+                                <button type='button'>Forgot Password?</button>
+                                <button type='button'>Login</button>
+                            </div>
+                        </form>
+                    </div>
                 </section>
                 <footer>
 
