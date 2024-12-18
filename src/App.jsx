71dbf34bb98fe4e98ccaa143158ca3d9ae0374e1
Faheemff <mfaheemweb@gmail.com>
2024-12-19 00:34:31 +0500

    Add

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,12 @@ import React from 'react'
 import Login from './Components/Auth/Login'
 import CreateAccount from './Components/Auth/CreateAccount'
 import { Route, Routes } from 'react-router-dom'
+import Header from './Components/Header'
 
 const App = () => {
   return (
     <div>
+      <Header />
       <Routes>
         <Route path='/createAccount' element={<CreateAccount />}/>
         <Route path='/loginAccount' element={<Login />}/>
--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,11 +1,16 @@
 // eslint-disable-next-line no-unused-vars
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Header = () => {
     return (
         <>
             <header className='px-10 py-6 bg-red-800 px-'>
                 <div><h1>Fah</h1></div>
+                <ul>
+                    <Link to="./Login.jsx" className='text-green-600 font-semibold'>Log in</Link>
+                    <Link to="./CreateAccount.jsx" className='text-green-600 font-semibold'>Sign up</Link>
+                </ul>
             </header>
         </>
     )
