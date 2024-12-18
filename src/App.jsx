2a6867b270f67130d970204d75e5e3dd8e8037a1
Faheemff <mfaheemweb@gmail.com>
2024-12-18 21:27:37 +0500

    add

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,12 @@
-// import React from 'react'
-
-import { useState } from "react"
-
-// import { useGSAP } from "@gsap/react"
-// import gsap from "gsap"
+// eslint-disable-next-line no-unused-vars
+import React from 'react'
+import Login from './Components/Auth/Login'
 
 const App = () => {
-  const [textshow, textHidden] =  useState(false);
-
-  const toggle = ()=> {
-    textHidden(textshow);
-  }
   return (
-    <main>
-      <div className='boxx'>
-        <div>{toggle}</div>
-        <button type="button" onClick={()=> setCont(count + 1)} >incress</button>
-      </div>
-    </main>
+    <div>
+      <Login />
+    </div>
   )
 }
 
---
+++ src/Components/Auth/Login.jsx
@@ -0,0 +1,24 @@
+// eslint-disable-next-line no-unused-vars
+import React from 'react'
+
+const Login = () => {
+    return (
+        <>
+            <main className='h-screen w-full bg-black text-white'>
+                <header className='px-10 py-6 bg-red-800 px-'>
+                    <div><h1>Faheem</h1></div>
+                </header>
+                <section className='flex justify-between w-full bg-white'>
+                    <div className='h-[500px] bg-green-600 w-[50%]'></div>
+                    <div className='h-[500px] bg-red-500 w-[50%]'></div>
+                </section>
+                <footer>
+
+                </footer>
+            </main>
+
+        </>
+    )
+}
+
+export default Login
