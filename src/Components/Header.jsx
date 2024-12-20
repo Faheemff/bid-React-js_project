e895a95a9cceadf82f945b1309dfab7da0e46364
Faheemff <mfaheemweb@gmail.com>
2024-12-20 22:36:16 +0500

    add

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -5,30 +5,30 @@ import { Link } from 'react-router-dom'
 const Header = () => {
     return (
         <>
-            <header className='px-10 py-2 flex justify-between items-center'>
+            <header className='px-10 py-1 flex justify-between items-center border-b-[1px] border-gray-300'>
                 <div className='logo'>
-                    <h1 className='font-[Kajiro] text-[50px] tracking-[0.1em]'>Faheem</h1>
+                    <h1 className='font-[Kajiro] text-[55px] tracking-[0.05em]'>Faheem</h1>
                 </div>
                 <div>
                     <ul className='flex items-center gap-5'>
                         <li>
                             <div>
-                                <a href="#">About</a>
+                                <a className='font-[Rimons] text-[18px]' href="#">About</a>
                             </div>
                         </li>
                         <li>
                             <div>
-                                <a href="#">Projects</a>
+                                <a className='font-[Rimons] text-[18px]' href="#">Projects</a>
                             </div>
                         </li>
                         <li>
                             <div>
-                                <a href="#">Contact</a>
+                                <a className='font-[Rimons] text-[18px]' href="#">Contact</a>
                             </div>
                         </li>
                         <li>
-                            <div className=''>
-                                <Link to="/loginAccount" className='bg-black text-white px-7 py-2 rounded-[50px] font-semibold active:scale-50'>Log in</Link>
+                            <div>
+                                <Link to="/loginAccount" className='bg-black text-white px-7 font-[Rimons] py-2 rounded-[50px] font-medium'>Log in</Link>
                             </div>
                         </li>
                     </ul>
